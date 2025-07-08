84215458e83b468a8763b726ba60b80d91758106
Rodolfo Hernández Villa <109832967+rodolfohv@users.noreply.github.com>
2025-07-08 04:01:07 +0000

    Entrega Ejercicio tarjeta perfil condicional

---
+++ src/script.js
@@ -0,0 +1,39 @@
+//if(18<20){
+//    console.log("ejemplo")
+//}else if (20 = 40) {
+//    console.log("ejemplo")
+//}else{
+//    console.log("ejemplo")
+//}
+
+//  Sintaxis Operador Ternario
+//    condicion ? valorSiEsVerdadero : valorSiEsFalso;
+
+//if (edad >= 18) {
+//    console.log("Eres mayor de edad");
+//else{
+//    console.log("Eres menor de edad");
+//}
+
+//let edad = 18;
+//let resultado = edad >= 18 ? "Eres mayor de edad" : "Eres menor de edad";
+
+
+
+//if (precio >= 20) {
+//    console.log("Es el precio justo");
+//}else if (precio == 45){
+//    console.log("El precio ha subido");
+//}else{
+//    "Tiene otro precio"
+//}
+
+//let precio = 20;
+//let resultadoPrecio = precio == 20 ? "El precio es de 20" : precio >= 21 ? "El precio ha subido" : "Tiene otro precio";
+
+
+// ´Texto aqui $(variableoExpresion) más texto´;
+
+let edad = 18;
+let nombre = "Lucía";
+console.log(´Hola mi nombre es $(nombre) y tengo $(edad) años.´)
